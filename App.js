786d118ff43cadecfb73a12c5b41736841a96e2d
GuitarSWP <47924424+GuitarSWP@users.noreply.github.com>
2019-09-22 01:55:51 +0700

    random

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
-const word = "sleep";
+var vocab = ["sleep","rest"]
+let random = parseInt(Math.random() *2)
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <WordCard value="sleep"/>
+        <WordCard value={vocab[random]}/>
       </div>
     );
   }
